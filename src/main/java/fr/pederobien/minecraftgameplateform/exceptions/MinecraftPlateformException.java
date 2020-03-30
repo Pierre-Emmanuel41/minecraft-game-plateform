772a59f057b47d033a3cfd94d989d23dbe7ef335
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-30 11:16:13 +0200

    Adding package exceptions

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/exceptions/MinecraftPlateformException.java
@@ -0,0 +1,9 @@
+package fr.pederobien.minecraftgameplateform.exceptions;
+
+public class MinecraftPlateformException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public MinecraftPlateformException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/fr/pederobien/minecraftgameplateform/exceptions/TimeTaskStateException.java
@@ -0,0 +1,9 @@
+package fr.pederobien.minecraftgameplateform.exceptions;
+
+public class TimeTaskStateException extends MinecraftPlateformException {
+	private static final long serialVersionUID = 1L;
+
+	public TimeTaskStateException(String message) {
+		super(message);
+	}
+}
