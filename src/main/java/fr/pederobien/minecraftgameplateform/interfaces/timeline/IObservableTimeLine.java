e678adaea7617f67fc77dda9c5f7decc51348e9c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-30 14:00:22 +0200

    Adding interfaces IObservableTimeLine, ITimeLineObserver and ITimeLinePeriodicObserver

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/timeline/IObservableTimeLine.java
@@ -0,0 +1,41 @@
+package fr.pederobien.minecraftgameplateform.interfaces.timeline;
+
+import java.time.LocalTime;
+
+public interface IObservableTimeLine {
+
+	/**
+	 * Register the given observer for the specific time. It is possible to add several observers for the same time. It that case,
+	 * each observer are be notified.
+	 * 
+	 * @param time The time at which the observer is notified.
+	 * @param obs  The observer to notify.
+	 */
+	void addObserver(LocalTime time, ITimeLineObserver obs);
+
+	/**
+	 * Remove the specified observer for the given time.
+	 * 
+	 * @param time The used as key to remove the observer.
+	 * @param obs  The observer to remove.
+	 */
+	void removeObserver(LocalTime time, ITimeLineObserver obs);
+
+	/**
+	 * Register periodically the given observer for the specific time. It is possible to add several periodic observers for the same
+	 * time. It that case, each observer are be notified. When notified, those observers will be notified at the time equals
+	 * currentTime + their period.
+	 * 
+	 * @param time The time at which the observer is notified.
+	 * @param obs  The observer to notify.
+	 */
+	void addObserver(LocalTime time, ITimeLinePeriodicObserver obs);
+
+	/**
+	 * Remove the specified observer for the given time.
+	 * 
+	 * @param time The used as key to remove the observer.
+	 * @param obs  The observer to remove.
+	 */
+	void removeObserver(ITimeLinePeriodicObserver obs);
+}
---
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/timeline/ITimeLineObserver.java
@@ -0,0 +1,13 @@
+package fr.pederobien.minecraftgameplateform.interfaces.timeline;
+
+import java.time.LocalTime;
+
+public interface ITimeLineObserver {
+
+	/**
+	 * Method called by the time line to notify that the time has changed.
+	 * 
+	 * @param time The current time.
+	 */
+	void timeChanged(LocalTime time);
+}
