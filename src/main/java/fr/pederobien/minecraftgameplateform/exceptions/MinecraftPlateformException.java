44d406e0a1454164b2818fd1a00639c76d225d65
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-31 19:41:03 +0200

    Adding default constructor in MinecraftPlateformException

--- src/main/java/fr/pederobien/minecraftgameplateform/exceptions/MinecraftPlateformException.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/exceptions/MinecraftPlateformException.java
@@ -3,6 +3,10 @@ package fr.pederobien.minecraftgameplateform.exceptions;
 public class MinecraftPlateformException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
+	public MinecraftPlateformException() {
+		super();
+	}
+
 	public MinecraftPlateformException(String message) {
 		super(message);
 	}
