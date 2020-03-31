ac1a1682bb61f76842c12a48b84f3b4df9ee623e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-31 11:24:02 +0200

    Adding interface IGame

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/element/IGame.java
@@ -0,0 +1,29 @@
+package fr.pederobien.minecraftgameplateform.interfaces.element;
+
+public interface IGame {
+
+	/**
+	 * @return True if the game is successful initiated, false otherwise.
+	 */
+	boolean initiate();
+
+	/**
+	 * Method called to start the game.
+	 */
+	void start();
+
+	/**
+	 * Method called to stop a game.
+	 */
+	void stop();
+
+	/**
+	 * Method called to pause the game.
+	 */
+	void pause();
+
+	/**
+	 * Method called to relaunch the game when it is in pause.
+	 */
+	void relaunch();
+}
