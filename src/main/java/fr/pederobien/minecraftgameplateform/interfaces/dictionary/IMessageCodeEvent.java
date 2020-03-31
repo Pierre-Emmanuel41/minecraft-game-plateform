b4c7ab9777e9aa87d5cfd7d1ba4de9fad102df4c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-31 19:03:58 +0200

    Adding method getPlugin in interface IMessageCodeEvent

--- src/main/java/fr/pederobien/minecraftgameplateform/interfaces/dictionary/IMessageCodeEvent.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/dictionary/IMessageCodeEvent.java
@@ -1,6 +1,13 @@
 package fr.pederobien.minecraftgameplateform.interfaces.dictionary;
 
+import org.bukkit.plugin.Plugin;
+
 public interface IMessageCodeEvent {
+
+	/**
+	 * @return The plugin to which the message code is associated.
+	 */
+	Plugin getPlugin();
 
 	/**
 	 * @return The code used to get the associated {@link IMessage}.
