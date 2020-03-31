dd16e2ea98a9229e113d085d185b47961a846d25
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-03-31 18:43:06 +0200

    Adding interface IDictionary

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/dictionary/IDictionary.java
@@ -0,0 +1,31 @@
+package fr.pederobien.minecraftgameplateform.interfaces.dictionary;
+
+import java.util.List;
+import java.util.Locale;
+
+public interface IDictionary {
+
+	/**
+	 * @return All locales supported by this dictionary. All locales should correspond to the same language. For instance :
+	 *         {@link Locale#ENGLISH}, {@link Locale#UK} etc...
+	 */
+	List<Locale> getLocales();
+
+	/**
+	 * Get the message associated to the message code from the message code event.
+	 * 
+	 * @param event The event used to get the message.
+	 * 
+	 * @return The associated message.
+	 */
+	String getMessage(IMessageCodeEvent event);
+
+	/**
+	 * Register the given message to this dictionary.
+	 * 
+	 * @param message The message to store.
+	 * 
+	 * @return This dictionary to register messages easier.
+	 */
+	IDictionary register(IMessage message);
+}
