7b7d73d9e59cb8a51871d83f068d49902ff292f2
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-01 22:57:43 +0200

    Updating class Plateform

--- src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
@@ -1,10 +1,13 @@
 package fr.pederobien.minecraftgameplateform.utils;
 
+import fr.pederobien.minecraftgameplateform.dictionary.NotificationCenter;
 import fr.pederobien.minecraftgameplateform.impl.element.GameConfigurationContext;
+import fr.pederobien.minecraftgameplateform.interfaces.dictionary.INotificationCenter;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfigurationContext;
 
 public class Plateform {
 	private static IGameConfigurationContext context = new GameConfigurationContext();
+	private static INotificationCenter center = NotificationCenter.getInstance();
 
 	public static String getVersion() {
 		return "v1.0-SNAPSHOT";
@@ -16,5 +19,12 @@ public class Plateform {
 	 */
 	public static IGameConfigurationContext getGameConfigurationContext() {
 		return context;
+	}
+
+	/**
+	 * @return The notification center to send message to player(s) that are currently logged into the server.
+	 */
+	public static INotificationCenter getNotificationCenter() {
+		return center;
 	}
 }
