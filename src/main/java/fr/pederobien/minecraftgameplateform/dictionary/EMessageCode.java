608243bd3e183f2686b142875af0b8c758085b59
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-03 00:34:28 +0200

    Adding message code STARTING_GAME

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/EMessageCode.java
@@ -0,0 +1,27 @@
+package fr.pederobien.minecraftgameplateform.dictionary;
+
+import fr.pederobien.minecraftgameplateform.interfaces.dictionary.IMessageCode;
+
+public enum EMessageCode implements IMessageCode {
+	STARTING_GAME;
+
+	private Permission permission;
+
+	private EMessageCode() {
+		this(Permission.OPERATORS);
+	}
+
+	private EMessageCode(Permission permission) {
+		this.permission = permission;
+	}
+
+	@Override
+	public Permission getPermission() {
+		return permission;
+	}
+
+	@Override
+	public void setPermission(Permission permission) {
+		this.permission = permission;
+	}
+}
