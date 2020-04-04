8f13de6d03e2dbed68376917a0a3721e540af05d
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-04 12:45:11 +0200

    Adding package xml in packages interfaces.element and impl.element

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/element/persistence/xml/IXmlPersistenceLoader.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecraftgameplateform.interfaces.element.persistence.xml;
+
+import org.w3c.dom.Node;
+
+import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IPersistenceLoader;
+import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnmodifiableNominable;
+
+public interface IXmlPersistenceLoader<T extends IUnmodifiableNominable> extends IPersistenceLoader<T> {
+
+	/**
+	 * Load data coming from the given root.
+	 * 
+	 * @param root The node that correspond to the root of an xml document.
+	 * 
+	 * @return this loader.
+	 */
+	IXmlPersistenceLoader<T> load(Node root);
+}
