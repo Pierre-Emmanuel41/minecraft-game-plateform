a11ffd04c5bfbe11cadb443792f2bf250963864a
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-04 16:24:40 +0200

    Adding interface IPersistenceEdition

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/editions/IPersistenceEdition.java
@@ -0,0 +1,12 @@
+package fr.pederobien.minecraftgameplateform.editions;
+
+import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IPersistence;
+import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnmodifiableNominable;
+
+public interface IPersistenceEdition<T extends IUnmodifiableNominable> {
+
+	/**
+	 * @return The persistence used to save/load objects associated to the type <code>T</code>.
+	 */
+	IPersistence<T> getPersistence();
+}
