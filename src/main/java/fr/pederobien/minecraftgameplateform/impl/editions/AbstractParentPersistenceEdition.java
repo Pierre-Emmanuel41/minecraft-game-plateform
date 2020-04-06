27b5eb83f295f95bfb2240ddff2f8f8cb146891b
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-06 14:43:16 +0200

    Adding method getPlugin in interface IParentPersistenceEdition

--- src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractParentPersistenceEdition.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractParentPersistenceEdition.java
@@ -11,10 +11,12 @@ import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnm
 public class AbstractParentPersistenceEdition<T extends IUnmodifiableNominable> extends AbstractGenericParentEdition<IMessageCode, T, IParentPersistenceEdition<T>>
 		implements IParentPersistenceEdition<T> {
 	private IPersistence<T> persistence;
+	private Plugin plugin;
 
 	public AbstractParentPersistenceEdition(String label, IMessageCode explanation, Plugin plugin, IPersistence<T> persistence) {
 		super(label, explanation, new ParentPersistenceHelper<T>(plugin));
 		this.persistence = persistence;
+		this.plugin = plugin;
 	}
 
 	@Override
@@ -30,5 +32,10 @@ public class AbstractParentPersistenceEdition<T extends IUnmodifiableNominable> 
 	@Override
 	public IPersistence<T> getPersistence() {
 		return persistence;
+	}
+
+	@Override
+	public Plugin getPlugin() {
+		return plugin;
 	}
 }
--- src/main/java/fr/pederobien/minecraftgameplateform/interfaces/editions/IParentPersistenceEdition.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/editions/IParentPersistenceEdition.java
@@ -1,5 +1,7 @@
 package fr.pederobien.minecraftgameplateform.interfaces.editions;
 
+import org.bukkit.plugin.Plugin;
+
 import fr.pederobien.minecraftdevelopmenttoolkit.interfaces.IGenericParentEdition;
 import fr.pederobien.minecraftgameplateform.interfaces.dictionary.IMessageCode;
 import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnmodifiableNominable;
@@ -7,4 +9,8 @@ import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnm
 public interface IParentPersistenceEdition<T extends IUnmodifiableNominable>
 		extends IGenericParentEdition<IMessageCode, T, IParentPersistenceEdition<T>>, IPersistenceEdition<T> {
 
+	/**
+	 * @return The plugin to which this parent persistence is associated to.
+	 */
+	Plugin getPlugin();
 }
