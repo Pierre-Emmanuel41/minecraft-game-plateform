b932f320b14d6983405b09003dddfc7e62d49abf
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-07 21:45:43 +0200

    Adding methods to send message from an AbstractParentPersistenceEdition

--- src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractParentPersistenceEdition.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractParentPersistenceEdition.java
@@ -1,14 +1,23 @@
 package fr.pederobien.minecraftgameplateform.impl.editions;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import fr.pederobien.minecraftdevelopmenttoolkit.exceptions.ArgumentNotFoundException;
 import fr.pederobien.minecraftdevelopmenttoolkit.impl.AbstractGenericParentEdition;
 import fr.pederobien.minecraftdevelopmenttoolkit.interfaces.IGenericMapEdition;
 import fr.pederobien.minecraftdevelopmenttoolkit.interfaces.IGenericParentEdition;
+import fr.pederobien.minecraftgameplateform.dictionary.messages.common.ECommonMessageCode;
 import fr.pederobien.minecraftgameplateform.interfaces.dictionary.IMessageCode;
+import fr.pederobien.minecraftgameplateform.interfaces.dictionary.IMessageEvent;
+import fr.pederobien.minecraftgameplateform.interfaces.dictionary.INotificationCenter;
 import fr.pederobien.minecraftgameplateform.interfaces.editions.IParentPersistenceEdition;
 import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IPersistence;
 import fr.pederobien.minecraftgameplateform.interfaces.element.unmodifiable.IUnmodifiableNominable;
+import fr.pederobien.minecraftgameplateform.utils.EventFactory;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
 
 public class AbstractParentPersistenceEdition<T extends IUnmodifiableNominable> extends AbstractGenericParentEdition<IMessageCode, T, IParentPersistenceEdition<T>>
 		implements IParentPersistenceEdition<T> {
@@ -19,6 +28,16 @@ public class AbstractParentPersistenceEdition<T extends IUnmodifiableNominable> 
 		super(label, explanation, new ParentPersistenceHelper<T>(plugin));
 		this.persistence = persistence;
 		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		try {
+			return super.onCommand(sender, command, label, args);
+		} catch (ArgumentNotFoundException e) {
+			sendMessageToSender(sender, ECommonMessageCode.COMMON_ARGUMENT_NOT_FOUND, e.getNotFoundArgument(), e.getLabel());
+		}
+		return false;
 	}
 
 	@Override
@@ -53,5 +72,32 @@ public class AbstractParentPersistenceEdition<T extends IUnmodifiableNominable> 
 	@Override
 	public Plugin getPlugin() {
 		return plugin;
+	}
+
+	/**
+	 * Send a message to the given player. First create an {@link IMessageEvent} that is used to get messages into registered
+	 * dictionaries for the given Plugin.
+	 * 
+	 * @param player The player to send a message.
+	 * @param plugin The plugin into the message is associated.
+	 * @param code   The code used to get the translation of the message in the player's language.
+	 * @param args   Arguments that could be useful to send dynamic messages.
+	 * 
+	 * @return The created message event.
+	 */
+	protected void sendMessageToSender(CommandSender sender, IMessageCode code, Object... args) {
+		if (sender instanceof Player)
+			getNotificationCenter().sendMessage(messageEvent((Player) sender, code, args));
+	}
+
+	private INotificationCenter getNotificationCenter() {
+		return Plateform.getNotificationCenter();
+	}
+
+	private IMessageEvent messageEvent(Player player, IMessageCode code, Object... args) {
+		String[] internalArgs = new String[args.length];
+		for (int i = 0; i < args.length; i++)
+			internalArgs[i] = args[i].toString();
+		return EventFactory.messageEvent(player, getPlugin(), code, internalArgs);
 	}
 }
