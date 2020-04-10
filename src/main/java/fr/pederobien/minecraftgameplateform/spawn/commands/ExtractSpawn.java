b0e84869cde1599945436fe71e7f66896b34dad0
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-10 22:53:34 +0200

    Upgrading class ExtractSpawn

--- src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/ExtractSpawn.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/ExtractSpawn.java
@@ -2,6 +2,7 @@ package fr.pederobien.minecraftgameplateform.spawn.commands;
 
 import org.bukkit.command.CommandSender;
 
+import fr.pederobien.minecraftgameplateform.commands.configurations.ECommonLabel;
 import fr.pederobien.minecraftgameplateform.commands.configurations.worldstructure.CommonExtract;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.worldstructure.spawn.ESpawnMessageCode;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ISpawn;
@@ -15,5 +16,6 @@ public class ExtractSpawn extends CommonExtract<ISpawn> {
 	@Override
 	protected void onExtracted(CommandSender sender, String name) {
 		sendMessageToSender(sender, ESpawnMessageCode.EXTRACT_SPAWN__SPAWN_EXTRACTED, name);
+		setNewEditionAvailable(ECommonLabel.SAVE);
 	}
 }
