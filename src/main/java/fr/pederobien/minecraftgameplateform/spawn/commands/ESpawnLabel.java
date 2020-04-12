27b55b87502ee4f76165a0a0af58137b7d794a21
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-12 17:40:28 +0200

    Adding label RANDOM in enumeration ESpawnLabel

--- src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/ESpawnLabel.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/ESpawnLabel.java
@@ -3,7 +3,7 @@ package fr.pederobien.minecraftgameplateform.spawn.commands;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum ESpawnLabel implements ILabel {
-	PLAYER_SPAWN("playerspawn");
+	PLAYER_SPAWN("playerspawn"), RANDOM("random");
 
 	private String label;
 
