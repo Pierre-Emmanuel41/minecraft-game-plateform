cd3f33894268a825ae993ff0a71d1638c25f0906
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-12 10:55:23 +0200

    Adding label CURRENT in enumeration ECommonLabel

--- src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/ECommonLabel.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/ECommonLabel.java
@@ -3,7 +3,7 @@ package fr.pederobien.minecraftgameplateform.commands.configurations;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum ECommonLabel implements ILabel {
-	NEW("new"), RENAME("rename"), SAVE("save"), DELETE("delete"), LIST("list");
+	NEW("new"), RENAME("rename"), SAVE("save"), DELETE("delete"), LIST("list"), CURRENT("current");
 
 	private String label;
 
