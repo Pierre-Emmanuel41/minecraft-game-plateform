2b79ea2048fa778722d2ec2a6878dc5a9679fc86
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-12 13:24:32 +0200

    Upgrading method onLaunched in class LaunchSpawn

--- src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/LaunchSpawn.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/spawn/commands/LaunchSpawn.java
@@ -2,6 +2,7 @@ package fr.pederobien.minecraftgameplateform.spawn.commands;
 
 import org.bukkit.command.CommandSender;
 
+import fr.pederobien.minecraftgameplateform.commands.configurations.ECommonLabel;
 import fr.pederobien.minecraftgameplateform.commands.configurations.worldstructure.CommonLaunch;
 import fr.pederobien.minecraftgameplateform.commands.configurations.worldstructure.EWorldStructureLabel;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.worldstructure.spawn.ESpawnMessageCode;
@@ -16,7 +17,9 @@ public class LaunchSpawn extends CommonLaunch<ISpawn> {
 	@Override
 	protected void onLaunched(CommandSender sender, String name, int x, int y, int z) {
 		sendMessageToSender(sender, ESpawnMessageCode.LAUNCH_SPAWN__SPAWN_LAUNCHED, name, x, y, z);
-		setNewEditionAvailable(EWorldStructureLabel.REMOVE);
+		setNewEditionsAvailable(ECommonLabel.CURRENT, ECommonLabel.DELETE, ECommonLabel.LIST, ECommonLabel.NEW, ECommonLabel.RENAME, ECommonLabel.SAVE);
+		setNewEditionsAvailable(EWorldStructureLabel.WORLD, EWorldStructureLabel.CENTER, EWorldStructureLabel.DIMENSION, EWorldStructureLabel.REMOVE,
+				EWorldStructureLabel.EXTRACT);
 	}
 
 	@Override
