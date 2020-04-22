00e6cd8eec4fc2a2740f2ee0f9fe1333a1a141c8
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-23 00:05:44 +0200

    Adding label LOAD in enumeration EGameConfigurationLabel

--- src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/EGameConfigurationLabel.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/EGameConfigurationLabel.java
@@ -3,7 +3,7 @@ package fr.pederobien.minecraftgameplateform.commands.configurations;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum EGameConfigurationLabel implements ILabel {
-	AS_CURRENT("ascurrent");
+	AS_CURRENT("ascurrent"), LOAD("load");
 
 	private String label;
 
