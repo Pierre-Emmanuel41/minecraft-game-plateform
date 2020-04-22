0c6f540893d4835efc33d22a3dbb4be13b1cf692
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-22 23:15:43 +0200

    Adding method getOrCreateConfigurationHelper in class Plateform

--- src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
@@ -3,10 +3,14 @@ package fr.pederobien.minecraftgameplateform.utils;
 import fr.pederobien.minecraftdictionary.impl.NotificationCenter;
 import fr.pederobien.minecraftdictionary.interfaces.INotificationCenter;
 import fr.pederobien.minecraftgameplateform.helpers.CommandHelper;
+import fr.pederobien.minecraftgameplateform.helpers.ConfigurationHelperManager;
 import fr.pederobien.minecraftgameplateform.impl.element.GameConfigurationContext;
 import fr.pederobien.minecraftgameplateform.interfaces.commands.ICommand;
 import fr.pederobien.minecraftgameplateform.interfaces.commands.ICommandHelper;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfiguration;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfigurationContext;
+import fr.pederobien.minecraftgameplateform.interfaces.helpers.IConfigurationHelperManager;
+import fr.pederobien.minecraftgameplateform.interfaces.helpers.IGameConfigurationHelper;
 import fr.pederobien.minecraftgameplateform.internal.IPersistenceCenter;
 import fr.pederobien.minecraftgameplateform.internal.PersistenceCenter;
 
@@ -15,6 +19,7 @@ public class Plateform {
 	private static INotificationCenter notificationCenter = NotificationCenter.getInstance();
 	private static ICommandHelper helper = CommandHelper.getInstance();
 	private static IPersistenceCenter persistenceCenter = PersistenceCenter.getInstance();
+	private static IConfigurationHelperManager configurationHelperManager = ConfigurationHelperManager.getInstance();
 
 	public static String getVersion() {
 		return "v1.0-SNAPSHOT";
@@ -48,5 +53,12 @@ public class Plateform {
 	 */
 	public static IPersistenceCenter getPersistenceCenter() {
 		return persistenceCenter;
+	}
+
+	/**
+	 * @return The manager used to or create an helper associated to a configuration.
+	 */
+	public static IGameConfigurationHelper getOrCreateConfigurationHelper(IGameConfiguration configuration) {
+		return configurationHelperManager.getOrCreateHelper(configuration);
 	}
 }
