59d42fa507dbb99ffb00ea6232c792027e7722dc
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-24 18:10:32 +0200

    Modifying default permission in ETeamMessageCode

--- src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/teams/add/ETeamAddMessageCode.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/teams/add/ETeamAddMessageCode.java
@@ -6,15 +6,16 @@ import fr.pederobien.minecraftdictionary.interfaces.IMessageCode;
 public enum ETeamAddMessageCode implements IMessageCode {
 	// Code for command add team
 	ADD_TEAM__EXPLANATION, ADD_TEAM__TEAM_NAME_IS_MISSING, ADD_TEAM__COLOR_NAME_IS_MISSING, ADD_TEAM__COLOR_DOES_NOT_EXIST, ADD_TEAM__TEAM_NAME_FORBIDDEN,
-	ADD_TEAM__TEAM_NAME_ALREADY_USED, ADD_TEAM__COLOR_ALREADY_USED, ADD_TEAM__TEAM_ADDED,
+	ADD_TEAM__TEAM_NAME_ALREADY_USED, ADD_TEAM__COLOR_ALREADY_USED, ADD_TEAM__TEAM_ADDED(Permission.ALL),
 
 	// Code for command add player
-	ADD_PLAYER__EXPLANATION, ADD_PLAYER__ANY_PLAYER_ADDED, ADD_PLAYER__ONE_PLAYER_ADDED, ADD_PLAYER__SEVERAL_PLAYERS_ADDED;
+	ADD_PLAYER__EXPLANATION, ADD_PLAYER__ANY_PLAYER_ADDED(Permission.ALL), ADD_PLAYER__ONE_PLAYER_ADDED(Permission.ALL),
+	ADD_PLAYER__SEVERAL_PLAYERS_ADDED(Permission.ALL);
 
 	private Permission permission;
 
 	private ETeamAddMessageCode() {
-		this(Permission.ALL);
+		this(Permission.OPERATORS);
 	}
 
 	private ETeamAddMessageCode(Permission permission) {
