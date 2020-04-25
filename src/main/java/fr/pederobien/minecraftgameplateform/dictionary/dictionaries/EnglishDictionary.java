7fd97c205c420a19fe1f8aabaaf80cef3340ad62
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-25 13:14:48 +0200

    Adding code REMOVE_PLAYER__ALL_PLAYERS_REMOVED in enumeration ETeamRemoveMessageCode

--- src/main/java/fr/pederobien/minecraftgameplateform/dictionary/dictionaries/EnglishDictionary.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/dictionaries/EnglishDictionary.java
@@ -38,6 +38,7 @@ import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.t
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.add.AddTeam_TeamNameAlreadyUsed;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.add.AddTeam_TeamNameForbidden;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.add.AddTeam_TeamNameIsMissing;
+import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.remove.RemovePlayer_AllPlayersRemoved;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.remove.RemovePlayer_Explanation;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.remove.RemoveTeam_AllTeamsRemoved;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.remove.RemoveTeam_AnyTeamRemoved;
@@ -207,5 +208,6 @@ public class EnglishDictionary extends AbstractDictionary {
 		register(RemoveTeam_OneTeamRemoved.ENGLISH);
 		register(RemoveTeam_SeveralTeamsRemoved.ENGLISH);
 		register(RemovePlayer_Explanation.ENGLISH);
+		register(RemovePlayer_AllPlayersRemoved.ENGLISH);
 	}
 }
---
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/teams/remove/RemovePlayer_AllPlayersRemoved.java
@@ -0,0 +1,31 @@
+package fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.remove;
+
+import fr.pederobien.minecraftdictionary.interfaces.IMessage;
+import fr.pederobien.minecraftgameplateform.dictionary.messages.AbstractNotDynamicMessage;
+
+public class RemovePlayer_AllPlayersRemoved extends AbstractNotDynamicMessage {
+	/**
+	 * Message in French when all players have been removed from their team.
+	 */
+	public static final IMessage FRENCH = new French();
+	/**
+	 * Message in English when all players have been removed from their team.
+	 */
+	public static final IMessage ENGLISH = new English();
+
+	public RemovePlayer_AllPlayersRemoved(String message) {
+		super(ETeamRemoveMessageCode.REMOVE_PLAYER__ALL_PLAYERS_REMOVED, message);
+	}
+
+	private static class French extends RemovePlayer_AllPlayersRemoved {
+		public French() {
+			super("Tous les joueurs ont été retirés de leurs équipes");
+		}
+	}
+
+	private static class English extends RemovePlayer_AllPlayersRemoved {
+		public English() {
+			super("All players have been removed from their team");
+		}
+	}
+}
