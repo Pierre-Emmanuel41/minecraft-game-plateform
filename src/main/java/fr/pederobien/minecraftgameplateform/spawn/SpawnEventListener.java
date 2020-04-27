469b95b4b9273113ea5cdd01656c58662e583728
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-04-27 23:07:29 +0200

    SpawnEventListener react to event CreatureSpawnEvent

--- src/main/java/fr/pederobien/minecraftgameplateform/spawn/SpawnEventListener.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/spawn/SpawnEventListener.java
@@ -1,13 +1,16 @@
 package fr.pederobien.minecraftgameplateform.spawn;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import fr.pederobien.minecraftgameplateform.impl.element.AbstractEventListener;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ISpawn;
 import fr.pederobien.minecraftmanagers.PlayerManager;
+import fr.pederobien.minecraftmanagers.WorldManager;
 
 public class SpawnEventListener extends AbstractEventListener {
 	private ISpawn spawn;
@@ -16,17 +19,32 @@ public class SpawnEventListener extends AbstractEventListener {
 		this.spawn = spawn;
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerJoiner(PlayerJoinEvent event) {
 		if (!isActivated())
 			return;
 		PlayerManager.teleporte(event.getPlayer(), spawn.getPlayerSpawn().getLocation());
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
 		if (!isActivated())
 			return;
 		event.setRespawnLocation(spawn.getPlayerSpawn().getLocation());
 	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		if (spawn.isRemoved() || spawn.isAllowMobsUnderSpawn() || !WorldManager.MOBS.contains(event.getEntityType()) || !isLocationUnderSpawn(event.getLocation()))
+			return;
+
+		event.setCancelled(true);
+	}
+
+	private boolean isLocationUnderSpawn(Location location) {
+		Location from = new Location(spawn.getWorld(), -spawn.getWidth() / 2 + spawn.getCenter().getX() - 20, 0, -spawn.getDepth() / 2 + spawn.getCenter().getZ() - 20);
+		Location to = new Location(spawn.getWorld(), spawn.getWidth() / 2 + spawn.getCenter().getX() + 20, 0, spawn.getDepth() / 2 + spawn.getCenter().getZ() + 20);
+		return WorldManager.isInAABB2D(location, from, to);
+	}
+
 }
