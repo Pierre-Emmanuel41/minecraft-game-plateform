feeb109ca83ae4a1d0f7ba803ed829b377557064
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-01 23:41:28 +0200

    Adding code LIST__ANY_EXISTING_TEAM in enumeration ETeamConfigurationMessageCode

--- src/main/java/fr/pederobien/minecraftgameplateform/dictionary/dictionaries/EnglishDictionary.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/dictionaries/EnglishDictionary.java
@@ -23,6 +23,7 @@ import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.S
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.ScoreboardRefresh_SeveralTicsDefined;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.ScoreboardRefresh_TicsNumberIsMissing;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.Add_Explanation;
+import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.List_AnyExistingTeam;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.List_Explanation;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.Modify_Explanation;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams.Remove_Explanation;
@@ -257,5 +258,6 @@ public class EnglishDictionary extends AbstractDictionary {
 		register(ModifyColor_ColorAlreadyUsed.ENGLISH);
 		register(ModifyColor_ColorUpdated.ENGLISH);
 		register(List_Explanation.ENGLISH);
+		register(List_AnyExistingTeam.ENGLISH);
 	}
 }
---
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/teams/List_AnyExistingTeam.java
@@ -0,0 +1,33 @@
+package fr.pederobien.minecraftgameplateform.dictionary.messages.configurations.teams;
+
+import fr.pederobien.minecraftdictionary.interfaces.IMessage;
+import fr.pederobien.minecraftgameplateform.dictionary.messages.AbstractDynamicMessage;
+
+public class List_AnyExistingTeam extends AbstractDynamicMessage {
+	/**
+	 * Message in French when there is any registered team for a configuration.
+	 */
+	public static final IMessage FRENCH = new French();
+	/**
+	 * Message in English when there is any registered team for a configuration.
+	 */
+	public static final IMessage ENGLISH = new English();
+
+	public List_AnyExistingTeam() {
+		super(ETeamConfigurationMessageCode.LIST__ANY_EXISTING_TEAM);
+	}
+
+	private static class French extends List_AnyExistingTeam {
+		@Override
+		public String getMessage(String... args) {
+			return "Il n'existe aucune équipe pour la configuration " + args[0];
+		}
+	}
+
+	private static class English extends List_AnyExistingTeam {
+		@Override
+		public String getMessage(String... args) {
+			return "There is any team for configuration " + args[0];
+		}
+	}
+}
