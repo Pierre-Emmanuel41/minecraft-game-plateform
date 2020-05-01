9ca93eb85bc05f04c3583f1a6240860dca4d6130
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-01 23:13:24 +0200

    Adding label LIST in enumeration ETeamConfigurationLabel

--- src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/teams/ETeamConfigurationLabel.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/teams/ETeamConfigurationLabel.java
@@ -3,7 +3,7 @@ package fr.pederobien.minecraftgameplateform.commands.configurations.teams;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
 
 public enum ETeamConfigurationLabel implements ILabel {
-	ADD("add"), REMOVE("remove"), MODIFY("modify");
+	ADD("add"), REMOVE("remove"), MODIFY("modify"), LIST("list");
 
 	private String label;
 
