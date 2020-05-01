3cc9040b539f5e86da487d1dc2b9c79dc263c3f1
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-01 17:03:03 +0200

    Adding enumeration ETeamModifyLabel

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/commands/configurations/teams/modify/ETeamModifyLabel.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecraftgameplateform.commands.configurations.teams.modify;
+
+import fr.pederobien.minecraftgameplateform.interfaces.element.ILabel;
+
+public enum ETeamModifyLabel implements ILabel {
+	NAME("name");
+
+	private String label;
+
+	private ETeamModifyLabel(String label) {
+		this.label = label;
+	}
+
+	@Override
+	public String getLabel() {
+		return label;
+	}
+}
