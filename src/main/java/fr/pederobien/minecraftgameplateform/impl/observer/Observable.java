fd7c2e935c9a2adcc8e198da094272fbb96d7067
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-02 18:24:03 +0200

    Adding interface IObservable and its implementation

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/observer/Observable.java
@@ -0,0 +1,41 @@
+package fr.pederobien.minecraftgameplateform.impl.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import fr.pederobien.minecraftgameplateform.interfaces.observer.IObservable;
+
+public class Observable<T> implements IObservable<T> {
+	private List<T> observers;
+
+	public Observable() {
+		observers = new ArrayList<T>();
+	}
+
+	@Override
+	public void addObserver(T obs) {
+		observers.add(obs);
+	}
+
+	@Override
+	public void removeObserver(T obs) {
+		observers.remove(obs);
+	}
+
+	@Override
+	public void notifyObservers(Consumer<T> consumer) {
+		internalNotify(observers.stream(), consumer);
+	}
+
+	@Override
+	public void notifyObservers(Predicate<T> predicate, Consumer<T> consumer) {
+		internalNotify(observers.stream().filter(predicate), consumer);
+	}
+
+	private void internalNotify(Stream<T> observers, Consumer<T> consumer) {
+		observers.peek(consumer);
+	}
+}
