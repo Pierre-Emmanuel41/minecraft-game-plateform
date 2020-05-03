635ee5b4f87e12f5eecc3fbbb6213279a6a72213
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-03 18:06:57 +0200

    Moving class AbstractPlateformDictionary in package impl.element

--- src/main/java/fr/pederobien/minecraftgameplateform/impl/element/AbstractPlateformDictionary.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/element/AbstractPlateformDictionary.java
@@ -1,9 +1,8 @@
-package fr.pederobien.minecraftgameplateform.impl;
+package fr.pederobien.minecraftgameplateform.impl.element;
 
 import java.util.Locale;
 
 import fr.pederobien.minecraftdictionary.impl.AbstractDictionary;
-import fr.pederobien.minecraftdictionary.interfaces.IMessage;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IMessages;
 
 public class AbstractPlateformDictionary extends AbstractDictionary {
@@ -14,7 +13,6 @@ public class AbstractPlateformDictionary extends AbstractDictionary {
 
 	protected void register(IMessages... list) {
 		for (IMessages messages : list)
-			for (IMessage message : messages.getMessages())
-				register(message);
+			messages.setDictionary(this);
 	}
 }
