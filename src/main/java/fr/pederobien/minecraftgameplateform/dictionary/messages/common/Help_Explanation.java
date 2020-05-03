00e9b614d765a41b75ba5ad7a52bef5e88a1a9bb
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-03 15:24:51 +0200

    Renaming class HelpExplanation as Help_Explanation

--- src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/common/Help_Explanation.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/common/Help_Explanation.java
@@ -2,21 +2,21 @@ package fr.pederobien.minecraftgameplateform.dictionary.messages.common;
 
 import fr.pederobien.minecraftgameplateform.dictionary.messages.AbstractNotDynamicMessage;
 
-public class HelpExplanation extends AbstractNotDynamicMessage {
+public class Help_Explanation extends AbstractNotDynamicMessage {
 
-	public HelpExplanation(String message) {
+	public Help_Explanation(String message) {
 		super(ECommonMessageCode.COMMON_HELP__EXPLANATION, message);
 		ECommonMessages.FRENCH.add(new French());
 		ECommonMessages.ENGLISH.add(new English());
 	}
 
-	private static class French extends HelpExplanation {
+	private static class French extends Help_Explanation {
 		public French() {
 			super("Pour afficher toutes les fonctionnalités et leurs explications");
 		}
 	}
 
-	private static class English extends HelpExplanation {
+	private static class English extends Help_Explanation {
 		public English() {
 			super("To display all features and their explanations");
 		}
