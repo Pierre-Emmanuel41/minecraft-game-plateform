47ebffd6bec5b933dcb64a615639532aa6cb70f3
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-03 12:18:24 +0200

    Deleting intermediate variables

--- src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/utils/Plateform.java
@@ -3,6 +3,8 @@ package fr.pederobien.minecraftgameplateform.utils;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 import fr.pederobien.minecraftdictionary.impl.NotificationCenter;
 import fr.pederobien.minecraftdictionary.interfaces.INotificationCenter;
 import fr.pederobien.minecraftgameplateform.helpers.CommandHelper;
@@ -13,7 +15,6 @@ import fr.pederobien.minecraftgameplateform.interfaces.commands.ICommandHelper;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfiguration;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfigurationContext;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IPluginManager;
-import fr.pederobien.minecraftgameplateform.interfaces.helpers.IConfigurationHelperManager;
 import fr.pederobien.minecraftgameplateform.interfaces.helpers.IGameConfigurationHelper;
 import fr.pederobien.minecraftgameplateform.internal.IPersistenceCenter;
 import fr.pederobien.minecraftgameplateform.internal.PersistenceCenter;
@@ -24,12 +25,6 @@ public class Plateform {
 	 */
 	public static final Path ROOT = Paths.get("plugins", "minecraft-game-plateform");
 	private static final String MINECRAFT_GAME_PLATEFORM = "minecraft-game-plateform";
-	private static IGameConfigurationContext gameContext = GameConfigurationContext.getInstance();
-	private static INotificationCenter notificationCenter = NotificationCenter.getInstance();
-	private static ICommandHelper commandHelper = CommandHelper.getInstance();
-	private static IPersistenceCenter persistenceCenter = PersistenceCenter.getInstance();
-	private static IConfigurationHelperManager configurationHelperManager = ConfigurationHelperManager.getInstance();
-	private static IPluginManager pluginManager = PluginManager.getInstance();
 
 	/**
 	 * @return The version of this plugin.
@@ -50,14 +45,14 @@ public class Plateform {
 	 *         configuration of this context.
 	 */
 	public static IGameConfigurationContext getGameConfigurationContext() {
-		return gameContext;
+		return GameConfigurationContext.getInstance();
 	}
 
 	/**
 	 * @return The notification center to send message to player(s) that are currently logged into the server.
 	 */
 	public static INotificationCenter getNotificationCenter() {
-		return notificationCenter;
+		return NotificationCenter.getInstance();
 	}
 
 	/**
@@ -65,27 +60,34 @@ public class Plateform {
 	 *         it is possible to not display it by setting {@link ICommand#setAvailable(boolean)} to false.
 	 */
 	public static ICommandHelper getCommandHelper() {
-		return commandHelper;
+		return CommandHelper.getInstance();
 	}
 
 	/**
 	 * @return The center used to track persistence version update.
 	 */
 	public static IPersistenceCenter getPersistenceCenter() {
-		return persistenceCenter;
+		return PersistenceCenter.getInstance();
 	}
 
 	/**
 	 * @return The manager used to or create an helper associated to a configuration.
 	 */
 	public static IGameConfigurationHelper getOrCreateConfigurationHelper(IGameConfiguration configuration) {
-		return configurationHelperManager.getOrCreateHelper(configuration);
+		return ConfigurationHelperManager.getInstance().getOrCreateHelper(configuration);
 	}
 
 	/**
 	 * @return The manager used to have access to all registered plugins to this plugin.
 	 */
 	public static IPluginManager getPluginManager() {
-		return pluginManager;
+		return PluginManager.getInstance();
+	}
+
+	/**
+	 * @return The plugin associated to this plateform.
+	 */
+	public static JavaPlugin getPlugin() {
+		return getPluginManager().getJavaPlugin(getName()).get();
 	}
 }
