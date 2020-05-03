3dae56c7c5a0f419d4f69aeeca274f7d598383fe
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-03 18:08:04 +0200

    Adding method setDictionary in interface IMessages

--- src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/EGameConfigurationMessages.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/dictionary/messages/configurations/EGameConfigurationMessages.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import fr.pederobien.minecraftdictionary.interfaces.IDictionary;
 import fr.pederobien.minecraftdictionary.interfaces.IMessage;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IMessages;
 
@@ -11,6 +12,7 @@ public enum EGameConfigurationMessages implements IMessages {
 	FRENCH, ENGLISH;
 
 	private List<IMessage> messages;
+	private IDictionary dictionary;
 
 	private EGameConfigurationMessages() {
 		messages = new ArrayList<IMessage>();
@@ -18,12 +20,23 @@ public enum EGameConfigurationMessages implements IMessages {
 
 	@Override
 	public void add(IMessage message) {
-		if (!messages.contains(message))
-			messages.add(message);
+		if (messages.contains(message))
+			return;
+
+		messages.add(message);
+		if (dictionary != null)
+			dictionary.register(message);
 	}
 
 	@Override
 	public List<IMessage> getMessages() {
 		return Collections.unmodifiableList(messages);
+	}
+
+	@Override
+	public void setDictionary(IDictionary dictionary) {
+		this.dictionary = dictionary;
+		for (IMessage message : messages)
+			dictionary.register(message);
 	}
 }
--- src/main/java/fr/pederobien/minecraftgameplateform/interfaces/element/IMessages.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/interfaces/element/IMessages.java
@@ -2,6 +2,7 @@ package fr.pederobien.minecraftgameplateform.interfaces.element;
 
 import java.util.List;
 
+import fr.pederobien.minecraftdictionary.interfaces.IDictionary;
 import fr.pederobien.minecraftdictionary.interfaces.IMessage;
 
 public interface IMessages {
@@ -17,4 +18,11 @@ public interface IMessages {
 	 * @return The list of registered messages. This list is unmodifiable.
 	 */
 	List<IMessage> getMessages();
+
+	/**
+	 * Set the dictionary to which this messages is associated. When a message is added, it is also added to the given dictionary
+	 * 
+	 * @param dictionary The dictionary associated to this messages.
+	 */
+	void setDictionary(IDictionary dictionary);
 }
