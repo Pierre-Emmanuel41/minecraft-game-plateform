7b7ba35989778711d7db8e08f202ca61fb288ce6
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-07 20:29:48 +0200

    Modifying super class

--- src/main/java/fr/pederobien/minecraftgameplateform/border/messages/WorldBorder_WorldDefined.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/border/messages/WorldBorder_WorldDefined.java
@@ -2,7 +2,6 @@ package fr.pederobien.minecraftgameplateform.border.messages;
 
 import fr.pederobien.minecraftdictionary.interfaces.IMessage;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.AbstractDynamicMessage;
-import fr.pederobien.minecraftgameplateform.spawn.messages.WorldSpawn_WorldDefined;
 
 public class WorldBorder_WorldDefined extends AbstractDynamicMessage {
 	public static final IMessage FRENCH = new French();
@@ -12,14 +11,14 @@ public class WorldBorder_WorldDefined extends AbstractDynamicMessage {
 		super(EBorderMessageCode.WORLD_BORDER__WORLD_DEFINED);
 	}
 
-	private static class French extends WorldSpawn_WorldDefined {
+	private static class French extends WorldBorder_WorldDefined {
 		@Override
 		public String getMessage(String... args) {
 			return "Le monde de la bordure " + args[0] + " est " + args[1];
 		}
 	}
 
-	private static class English extends WorldSpawn_WorldDefined {
+	private static class English extends WorldBorder_WorldDefined {
 		@Override
 		public String getMessage(String... args) {
 			return "The " + args[0] + " border's world is " + args[1];
