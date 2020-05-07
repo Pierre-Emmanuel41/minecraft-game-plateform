67c2b4bfde7484250716417bfb993f62a0c52da5
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-07 23:32:36 +0200

    Fix bad copy/paste

--- src/main/java/fr/pederobien/minecraftgameplateform/border/messages/ListBorder_NoElement.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/border/messages/ListBorder_NoElement.java
@@ -2,14 +2,13 @@ package fr.pederobien.minecraftgameplateform.border.messages;
 
 import fr.pederobien.minecraftdictionary.interfaces.IMessage;
 import fr.pederobien.minecraftgameplateform.dictionary.messages.AbstractNotDynamicMessage;
-import fr.pederobien.minecraftgameplateform.spawn.messages.ESpawnMessageCode;
 
 public class ListBorder_NoElement extends AbstractNotDynamicMessage {
 	public static final IMessage FRENCH = new French();
 	public static final IMessage ENGLISH = new English();
 
 	public ListBorder_NoElement(String message) {
-		super(ESpawnMessageCode.LIST_SPAWN__NO_ELEMENT, message);
+		super(EBorderMessageCode.LIST_BORDER__NO_ELEMENT, message);
 	}
 
 	private static class French extends ListBorder_NoElement {
