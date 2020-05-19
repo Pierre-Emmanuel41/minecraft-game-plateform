a3b4c47dc34527dd7f6cd442da1d0f0d63189e84
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-19 18:53:28 +0200

    Updating project du to update in project fr.pedrobien.minecraftdevelopmenttoolkit

--- src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractMapPersistenceEdition.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractMapPersistenceEdition.java
@@ -6,24 +6,17 @@ import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.StringJoiner;
 
-import org.bukkit.GameRule;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import fr.pederobien.dictionary.interfaces.IDictionaryManager;
 import fr.pederobien.minecraftdevelopmenttoolkit.impl.messagecode.AbstractMessageCodeMapEdition;
-import fr.pederobien.minecraftdictionary.impl.MinecraftMessageEvent;
 import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageCode;
-import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageEvent;
 import fr.pederobien.minecraftdictionary.interfaces.IMinecraftNotificationCenter;
 import fr.pederobien.minecraftgameplateform.helpers.DisplayHelper;
 import fr.pederobien.minecraftgameplateform.interfaces.editions.IMapPersistenceEdition;
 import fr.pederobien.minecraftgameplateform.interfaces.editions.IParentPersistenceEdition;
 import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IMinecraftPersistence;
 import fr.pederobien.minecraftgameplateform.utils.Plateform;
-import fr.pederobien.minecraftmanagers.WorldManager;
 import fr.pederobien.persistence.interfaces.IUnmodifiableNominable;
 
 public abstract class AbstractMapPersistenceEdition<T extends IUnmodifiableNominable>
@@ -57,6 +50,11 @@ public abstract class AbstractMapPersistenceEdition<T extends IUnmodifiableNomin
 		return this;
 	}
 
+	@Override
+	protected IMinecraftNotificationCenter getNotificationCenter() {
+		return Plateform.getNotificationCenter();
+	}
+
 	/**
 	 * Get the persistence coming from the parent of this edition. It is equivalent to call {@link #getParent()} and then
 	 * {@link IParentPersistenceEdition#getPersistence()}.
@@ -65,55 +63,6 @@ public abstract class AbstractMapPersistenceEdition<T extends IUnmodifiableNomin
 	 */
 	protected IMinecraftPersistence<T> getPersistence() {
 		return getParent().getPersistence();
-	}
-
-	/**
-	 * Send a message to the given player. First create an {@link IMinecraftMessageEvent} that is used to get messages into registered
-	 * dictionaries for the given Plugin. This method is synchronized with {@link GameRule#SEND_COMMAND_FEEDBACK}. This means that if
-	 * the game rule has been set to false, then no message is send.
-	 * 
-	 * @param sender Generally a player, it is used to get a message in his language.
-	 * @param plugin The plugin into the message is associated.
-	 * @param code   The code used to get the translation of the message in the player's language.
-	 * @param args   Arguments that could be useful to send dynamic messages.
-	 * 
-	 * @return The created message event.
-	 */
-	protected void sendMessageToSender(CommandSender sender, IMinecraftMessageCode code, Object... args) {
-		if (sender instanceof Player && WorldManager.OVERWORLD.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK))
-			getNotificationCenter().sendMessage(messageEvent((Player) sender, code, args));
-	}
-
-	/**
-	 * Send a message to the given player. First create an {@link IMinecraftMessageEvent} that is used to get messages into registered
-	 * dictionaries for the given Plugin. This method send a message even if the game rule {@link GameRule#SEND_COMMAND_FEEDBACK} is
-	 * set to false.
-	 * 
-	 * @param sender Generally a player, it is used to get a message in his language.
-	 * @param plugin The plugin into the message is associated.
-	 * @param code   The code used to get the translation of the message in the player's language.
-	 * @param args   Arguments that could be useful to send dynamic messages.
-	 * 
-	 * @return The created message event.
-	 * 
-	 * @see #sendMessageToSender(CommandSender, IMinecraftMessageCode, Object...)
-	 */
-	protected void sendMessageToSenderNotSynchonized(CommandSender sender, IMinecraftMessageCode code, Object... args) {
-		if (sender instanceof Player && WorldManager.OVERWORLD.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK))
-			getNotificationCenter().sendMessage(messageEvent((Player) sender, code, args));
-	}
-
-	/**
-	 * Get a message corresponding to the given message code.
-	 * 
-	 * @param sender Generally a player, it is used to get a message in his language.
-	 * @param code   The code used to get the translation of the message in the player's language.
-	 * @param args   Arguments that could be useful to send dynamic messages.
-	 * 
-	 * @return The message associated to the specified code. If the sender is not a player, then it returns an empty string.
-	 */
-	protected String getMessageFromDictionary(CommandSender sender, IMinecraftMessageCode code, Object... args) {
-		return sender instanceof Player ? getMessage((Player) sender, code, args) : "";
 	}
 
 	/**
@@ -406,23 +355,5 @@ public abstract class AbstractMapPersistenceEdition<T extends IUnmodifiableNomin
 	 */
 	protected String toString(Location location, boolean full) {
 		return DisplayHelper.toString(location, full);
-	}
-
-	/**
-	 * @return The notification center to send message to player(s) that are currently logged into the server.
-	 */
-	private IMinecraftNotificationCenter getNotificationCenter() {
-		return Plateform.getNotificationCenter();
-	}
-
-	private IMinecraftMessageEvent messageEvent(Player player, IMinecraftMessageCode code, Object... args) {
-		String[] internalArgs = new String[args.length];
-		for (int i = 0; i < args.length; i++)
-			internalArgs[i] = args[i].toString();
-		return new MinecraftMessageEvent(player, code, (Object[]) internalArgs);
-	}
-
-	private String getMessage(Player player, IMinecraftMessageCode code, Object... args) {
-		return ((IDictionaryManager) getNotificationCenter().getDictionaryContext()).getMessage(messageEvent(player, code, args));
 	}
 }
