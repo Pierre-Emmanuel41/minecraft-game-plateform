a8379c719b97ed86868ea64a1268f84ef177187d
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-21 10:52:42 +0200

    The spawn listener is deactivated when a game is started

--- src/main/java/fr/pederobien/minecraftgameplateform/spawn/SpawnCommand.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/spawn/SpawnCommand.java
@@ -3,6 +3,7 @@ package fr.pederobien.minecraftgameplateform.spawn;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.minecraftgameplateform.commands.AbstractParentCommand;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IGame;
 import fr.pederobien.minecraftgameplateform.interfaces.element.ISpawn;
 import fr.pederobien.minecraftgameplateform.spawn.commands.SpawnParent;
 
@@ -10,5 +11,10 @@ public class SpawnCommand extends AbstractParentCommand<ISpawn> {
 
 	public SpawnCommand(JavaPlugin plugin) {
 		super(plugin, new SpawnParent(plugin));
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsStarted(U IGame) {
+		getParent().get().getListener().setActivated(false);
 	}
 }
