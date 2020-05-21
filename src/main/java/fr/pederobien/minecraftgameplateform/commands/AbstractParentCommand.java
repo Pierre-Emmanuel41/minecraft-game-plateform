020e2362e58e33ecce58287f179670c9e823b0a9
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-05-21 10:24:53 +0200

    Updating interface IParentCommand

--- src/main/java/fr/pederobien/minecraftgameplateform/commands/AbstractParentCommand.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/commands/AbstractParentCommand.java
@@ -7,10 +7,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.pederobien.minecraftgameplateform.interfaces.commands.IParentCommand;
 import fr.pederobien.minecraftgameplateform.interfaces.editions.IParentPersistenceEdition;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IGame;
 import fr.pederobien.minecraftgameplateform.utils.Plateform;
 import fr.pederobien.persistence.interfaces.IUnmodifiableNominable;
 
-public class AbstractParentCommand<T extends IUnmodifiableNominable> extends AbstractCommand implements IParentCommand<T> {
+public abstract class AbstractParentCommand<T extends IUnmodifiableNominable> extends AbstractCommand implements IParentCommand<T> {
 	private IParentPersistenceEdition<T> parent;
 
 	protected AbstractParentCommand(JavaPlugin plugin, IParentPersistenceEdition<T> parent) {
@@ -33,5 +34,45 @@ public class AbstractParentCommand<T extends IUnmodifiableNominable> extends Abs
 	@Override
 	public IParentPersistenceEdition<T> getParent() {
 		return parent;
+	}
+
+	@Override
+	public <U extends IGame> boolean onGameIsStarting(U IGame) {
+		return true;
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsStarted(U IGame) {
+
+	}
+
+	@Override
+	public <U extends IGame> boolean onGameIsPausing(U IGame) {
+		return true;
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsPaused(U IGame) {
+
+	}
+
+	@Override
+	public <U extends IGame> boolean onGameIsResuming(U IGame) {
+		return true;
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsResumed(U IGame) {
+
+	}
+
+	@Override
+	public <U extends IGame> boolean onGameIsStopping(U IGame) {
+		return true;
+	}
+
+	@Override
+	public <U extends IGame> void onGameIsStopped(U IGame) {
+
 	}
 }
