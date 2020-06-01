d9f485ef990c703b28f82a7be8c3ea6ce80d3748
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-01 19:36:56 +0200

    Removing override of method forceUpdate in class BorderPersistence

--- src/main/java/fr/pederobien/minecraftgameplateform/border/persistence/BorderPersistence.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/border/persistence/BorderPersistence.java
@@ -71,9 +71,4 @@ public class BorderPersistence extends AbstractMinecraftPersistence<IBorderConfi
 		saveDocument(doc, get().getName());
 		return false;
 	}
-
-	@Override
-	public boolean forceUpdate() {
-		return true;
-	}
 }
