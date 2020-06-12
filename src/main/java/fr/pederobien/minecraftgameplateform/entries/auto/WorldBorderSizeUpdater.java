7217a86af8a561be27d2fb715e51494214c3579c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-12 12:50:40 +0200

    Adding class WorldBorderSizeUpdater

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/entries/auto/WorldBorderSizeUpdater.java
@@ -0,0 +1,43 @@
+package fr.pederobien.minecraftgameplateform.entries.auto;
+
+import fr.pederobien.minecraftgameplateform.entries.simple.WorldBorderSizeEntry;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IBorderConfiguration;
+import fr.pederobien.minecraftscoreboards.interfaces.ISimpleObjective;
+
+public class WorldBorderSizeUpdater extends BorderConfigurationEntryUpdater<WorldBorderSizeEntry> {
+
+	/**
+	 * Create an entry updater. This entry is responsible to update the source entry.
+	 * 
+	 * @param objective The objective associated to the source entry.
+	 * @param source    The source tracked by this updater.
+	 */
+	public WorldBorderSizeUpdater(ISimpleObjective objective, WorldBorderSizeEntry source) {
+		super(objective, source);
+	}
+
+	/**
+	 * Create an entry updater. This entry is responsible to update the source entry.
+	 * 
+	 * @param objective       The objective associated to the source entry.
+	 * @param score           The line number of this entry in the player objective.
+	 * @param configuration   The configuration used to get informations to display.
+	 * @param pattern         A string used to format the world border size.
+	 * @param displayHalfSize True to display the radius, false to display the diameter.
+	 */
+	public WorldBorderSizeUpdater(ISimpleObjective objective, int score, IBorderConfiguration configuration, String pattern, boolean displayHalfSize) {
+		this(objective, new WorldBorderSizeEntry(score, configuration, pattern).setDisplayHalfSize(displayHalfSize));
+	}
+
+	/**
+	 * Create an entry updater. This entry is responsible to update the source entry.
+	 * 
+	 * @param objective     The objective associated to the source entry.
+	 * @param score         The line number of this entry in the player objective.
+	 * @param configuration The configuration used to get informations to display.
+	 * @param pattern       A string used to format the world border size.
+	 */
+	public WorldBorderSizeUpdater(ISimpleObjective objective, int score, IBorderConfiguration configuration, String pattern) {
+		this(objective, score, configuration, pattern, false);
+	}
+}
