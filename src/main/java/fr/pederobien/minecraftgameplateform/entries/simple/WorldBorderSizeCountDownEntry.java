5a7057cb704a6f3137276c09888a0432c1c129e6
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-15 14:14:07 +0200

    Adding classes WorldBorderSizeCountDownEntry and WorldBorderSizeCountDownAutoUpdater

---
+++ src/main/java/fr/pederobien/minecraftgameplateform/entries/simple/WorldBorderSizeCountDownEntry.java
@@ -0,0 +1,39 @@
+package fr.pederobien.minecraftgameplateform.entries.simple;
+
+import java.time.LocalTime;
+
+import org.bukkit.entity.Player;
+
+import fr.pederobien.minecraftdevelopmenttoolkit.utils.DisplayHelper;
+import fr.pederobien.minecraftgameplateform.interfaces.element.IBorderConfiguration;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
+
+public class WorldBorderSizeCountDownEntry extends WorldBorderSizeEntry {
+	private boolean isCountDownEquals0;
+
+	public WorldBorderSizeCountDownEntry(int score, IBorderConfiguration configuration, String pattern) {
+		super(score, configuration, pattern);
+		isCountDownEquals0 = false;
+	}
+
+	private LocalTime countDown;
+
+	@Override
+	protected String updateCurrentValue(Player player) {
+		String current = "";
+		if (!isCountDownEquals0) {
+			countDown = getConfiguration().getStartTime().minusSeconds(Plateform.getTimeTask().getGameTime().toSecondOfDay());
+			if (!countDown.equals(LocalTime.of(0, 0, 0)))
+				current += DisplayHelper.toString(countDown, false) + " | ";
+			else
+				isCountDownEquals0 = true;
+		}
+		current += super.updateCurrentValue(player);
+		return current;
+	}
+
+	@Override
+	public WorldBorderSizeCountDownEntry setDisplayHalfSize(boolean displayHalfSize) {
+		return (WorldBorderSizeCountDownEntry) super.setDisplayHalfSize(displayHalfSize);
+	}
+}
