a0fab59ed5f5c12216a78af81632264e622ef712
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-08-17 21:53:15 +0200

    Adding class AbstractConfigurationParentPersistenceEdition

--- src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractGameConfigurationParent.java
+++ src/main/java/fr/pederobien/minecraftgameplateform/impl/editions/AbstractGameConfigurationParent.java
@@ -3,83 +3,17 @@ package fr.pederobien.minecraftgameplateform.impl.editions;
 import org.bukkit.plugin.Plugin;
 
 import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageCode;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonDetails;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonDelete;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonList;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonLoad;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonNew;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonRename;
-import fr.pederobien.minecraftgameplateform.commands.common.CommonSave;
 import fr.pederobien.minecraftgameplateform.commands.configurations.GameConfigurationEditionFactory;
-import fr.pederobien.minecraftgameplateform.interfaces.editions.IMapPersistenceEdition;
 import fr.pederobien.minecraftgameplateform.interfaces.element.IGameConfiguration;
 import fr.pederobien.minecraftgameplateform.interfaces.element.persistence.IMinecraftPersistence;
 
-public abstract class AbstractGameConfigurationParent<T extends IGameConfiguration> extends AbstractParentPersistenceEdition<T> {
+public abstract class AbstractGameConfigurationParent<T extends IGameConfiguration> extends AbstractConfigurationParentPersistenceEdition<T> {
 
 	public AbstractGameConfigurationParent(String label, IMinecraftMessageCode explanation, Plugin plugin, IMinecraftPersistence<T> persistence) {
 		super(label, explanation, plugin, persistence);
 
-		addEdition(getNewEdition());
-		addEdition(getRenameEdition());
-		addEdition(getSaveEdition());
-		addEdition(getListEdition());
-		addEdition(getDeleteEdition());
-		addEdition(getCurrentEdition());
-		addEdition(getLoadEdition());
 		addEdition(GameConfigurationEditionFactory.asCurrentEdition());
 		addEdition(GameConfigurationEditionFactory.teamEdition());
 		addEdition(GameConfigurationEditionFactory.pvpTime());
 	}
-
-	// Specific to create, rename, save, delete or display the list of game configuration.
-
-	/**
-	 * The returned edition should extends {@link CommonNew} for more security.
-	 * 
-	 * @return An edition to create a new game configuration.
-	 */
-	protected abstract IMapPersistenceEdition<T> getNewEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonRename} for more security.
-	 * 
-	 * @return An edition to rename a game configuration.
-	 */
-	protected abstract IMapPersistenceEdition<T> getRenameEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonSave} for more security.
-	 * 
-	 * @return An edition to save game configuration informations.
-	 */
-	protected abstract IMapPersistenceEdition<T> getSaveEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonList} for more security.
-	 * 
-	 * @return An edition to display each existing game configuration name.
-	 */
-	protected abstract IMapPersistenceEdition<T> getListEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonDelete} for more security.
-	 * 
-	 * @return An edition to delete game configuration file.
-	 */
-	protected abstract IMapPersistenceEdition<T> getDeleteEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonDetails} for more security.
-	 * 
-	 * @return An edition to display current configuration's characteristics.
-	 */
-	protected abstract IMapPersistenceEdition<T> getCurrentEdition();
-
-	/**
-	 * The returned edition should extends {@link CommonLoad} for more security.
-	 * 
-	 * @return An edition to load a game configuration in order to modify it.
-	 */
-	protected abstract IMapPersistenceEdition<T> getLoadEdition();
 }
