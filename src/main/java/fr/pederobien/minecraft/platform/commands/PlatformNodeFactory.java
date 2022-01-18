6542237e51488447eb9749f4509592be1043c1fe
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-01-18 10:39:40 +0100

    Fix: NullPointerException for a ConfigurableNode

--- src/main/java/fr/pederobien/minecraft/platform/commands/PlatformNodeFactory.java
+++ src/main/java/fr/pederobien/minecraft/platform/commands/PlatformNodeFactory.java
@@ -1,18 +1,20 @@
 package fr.pederobien.minecraft.platform.commands;
 
+import java.util.function.Supplier;
+
 import fr.pederobien.minecraft.platform.commands.common.NodeBuilderFactory;
 import fr.pederobien.minecraft.platform.interfaces.IPvpTimeConfigurable;
 
 public class PlatformNodeFactory extends NodeBuilderFactory {
 
 	/**
-	 * Creates a node in order to modify the PVP time for this configuration.
+	 * Creates a node in order to modify the PVP time for a pvp time configurable.
 	 * 
-	 * @param configuration The configuration that contains a PVP time parameter.
+	 * @param pvpTime The configuration that contains a PVP time parameter.
 	 * 
 	 * @return A node that modify the PVP time value.
 	 */
-	public PvpTimeNode pvpTimeNode(IPvpTimeConfigurable configuration) {
-		return new PvpTimeNode(configuration);
+	public static PvpTimeNode pvpTimeNode(Supplier<IPvpTimeConfigurable> pvpTime) {
+		return new PvpTimeNode(() -> pvpTime.get());
 	}
 }
